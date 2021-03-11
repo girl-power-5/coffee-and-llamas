84264efd10bb9cf38b4cc1fd1f1622d9c3c7fd1f
Claudia Alpert <claudiaalpert@gmail.com>
2021-03-10 19:14:29 -0600

    edited app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,105 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import API from "./utils/API";
-
+import { UserContext } from './UserContext';
+import Register from './components/Register';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
 function App() {
-
+  const [userStatus, setUserStatus] = useState({
+                                        isLoggedIn: false,
+                                        username: null,
+                                        password: null,
+                                      })
   useEffect(() => { 
     API.getData()
       .then(res => console.log('TEST', res))
       .catch(err => console.log(err))
    }, []);
-
+   useEffect(() => {
+    getUser();
+   }, [])
+   const updateUser = (updatedProp, update) => {
+    setUserStatus({ ...userStatus, [updatedProp]: update });
+  }
+   const getUser = () => {
+    axios.get('/user/').then(response => {
+      if (response.data.user) {
+        setUserStatus({
+          isLoggedIn: true,
+          username: response.data.user.username
+        });
+      } else {
+        setUserStatus({
+          isLoggedIn: false,
+          username: null
+        });
+      }
+    });
+  }
+  const register = async () => {
+		const { username, password } = userStatus;
+		let response = await axios.post('/user/register', { username, password });
+		if (response.status === 200) {
+			setUserStatus({ isLoggedIn: true });
+      // window.history.push("/");
+		} else {
+			console.log('signup error');
+		}
+	}
+  const login = async () => {
+    const { username, password } = userStatus;
+    let response = await axios.post('/user/login/', { username, password });
+    if (response.status === 200) {
+      setUserStatus({ isLoggedIn: true });
+      // window.history.push("/");
+    } else {
+      console.log('login error');
+    }
+  }
+  const logout = () => {
+    axios.post('/user/logout').then(response => {
+      if (response.status === 200) {
+        setUserStatus({
+          isLoggedIn: false,
+          username: null,
+          password: null
+        });
+        // window.history.push("/");
+      }
+    }).catch(error => {
+      console.log('logout error', error);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <div className="App">
+        <Navbar isLoggedIn={userStatus.isLoggedIn} logout={logout} />
+        <Route exact path="/" render={() => <Home isLoggedIn={userStatus.isLoggedIn} username={userStatus.username} />} />
+        <Route path="/login" render={() => (
+          <Login 
+            username={userStatus.username} 
+            password={userStatus.password}
+            updateUser={updateUser} 
+            login={login} 
+          />
+        )} />
+        <Route path="/register" render={() => (
+          <Register 
+            username={userStatus.username} 
+            password={userStatus.password}
+            updateUser={updateUser} 
+            register={register}
+          />
+        )} />
+        <Route path="/page" render={() => <Dashboard isLoggedIn={userStatus.isLoggedIn} username={userStatus.username} />} />
+      </div>
+      </Router>
   );
 }
-
 export default App;
+
