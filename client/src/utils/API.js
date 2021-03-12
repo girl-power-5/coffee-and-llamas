9daaa9e2e131aab58e4474197a2adc57d53b7753
Emily Blair <emilyblair96@gmail.com>
2021-03-12 14:55:27 -0600

    Set up frontend post route for registration

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 export default {
-  getData: function() {
-    return axios.get('/api')
-  }, 
   handleRegistration: function(profileData) {
-    return axios.post('/api/newregistration', profileData)
+    return axios.post('/api/users/newregistration', profileData)
   }
 }
