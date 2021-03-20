c9fb690a5f407fd15191d2bb080b0b81d424c4c4
amberchiodini <69092983+amberchiodini@users.noreply.github.com>
2021-03-20 14:09:42 -0500

    Resized logo & emboldend p tag

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -5,8 +5,7 @@ export default function LandingPage() {
   return ( 
     <div>
   <div class="jumbotron">
-  <h1 class="display-3">Welcome!</h1>
-  <p class="lead">The Future of Safety</p>
+  <p class="lead" style={{fontWeight: "bold"}}>The Future of Safety</p>
   <p class="lead">
   <Link to="/login"><button type="button" class="btn btn-primary mr-2">Login</button></Link>
   <Link to="/register"><button type="button" class="btn btn-primary">Register</button></Link>
