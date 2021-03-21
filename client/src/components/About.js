e4106609d0882aa578d3625a4e3b0510623c9e2b
Claudia Alpert <claudiaalpert@gmail.com>
2021-03-21 17:02:58 -0500

    added about.js

---
+++ client/src/components/About.js
@@ -0,0 +1,65 @@
+import React from "react" 
+
+
+export default function About () {
+
+    return (
+        <div>
+            <h1>About Us!</h1>
+            <br/>
+            <article>
+            <h3>The IMOK Story</h3>
+            <p>As the pandemic comes to an end, the world will once again become a place for all to explore. There will be a time where humans are no longer threatened by a deadly virus and we will all live in peace and harmony. Just kidding! Sure we may not have to worry about social distancing, but let's be honest, this world will always be a scary place for many. </p>
+            <p>Our group would occasionally discuss the fascination a lot of people have with serial-killer documentaries including ourselves. This of course led to the conversation about online dating and how safe it really is. As an all-female group, each of us has felt the fear of going out alone.</p>
+            <p> When discussing the precautions we would usually take when going out or meeting a stranger, one woman said, "I never contact my friends when I get home...". We all paused and looked at each other. It's unfortunate, but we have been taught that going outside alone at night is not a safe thing to do and that you should always notify a friend when you're home safe. </p>
+            <p>This sparked an idea: what if there was an app for that? An app specifically made to be able to quickly and easily notify someone incase something were to happen when no one's around. With these thoughts in mind, IMOK was born. </p>
+            <br/>
+            <h3>App Description</h3>
+            <p>An application that gives the user the ability to let others know their current location when they go out so that if something were to happen, someone would be notified. A user will create a Safety Squad of up to 5 individuals who they are closest with, that they would call in case of an emergency, or say a really bad date.  Each squad member will include a name and phone number.</p>
+            <p>The user would then create events, outings, etc.  They would choose the day, time, and location of the meeting and most importantly who they are with and a link to that individuals social media.  This information will display on the page.  Their squad would be notified of the event being created.</p>
+            <p>The day of the event the user can then check into the event when they arrive, to let their squad know by clicking a button on the event page.   The user has several options to choose from to provide status update.</p>
+
+            <br/>
+            <h3>Alert Messages</h3>
+            <ul>
+                <li>Arrived: Msg from IMOK squad member (name): Made it to the meeting spot</li>
+                <li>Bored:  Msg from IMOK squad member (name): Bored but IMOK</li>
+                <li>Good:  Msg from IMOK squad member (name): Having a good time, signing off!</li>
+                <li>Standby:  Msg from IMOK squad member (name): Feeling uncomfortable, be on Standby!</li>
+                <li>SOS: Msg from IMOK squad member (name): SOS feel unsafe!</li>
+                <li>Safe:  Msg from IMOK squad member (name): Made it home/somewhere, am safe for the night!</li>
+                <li>Morning: Msg from IMOK squad member (name):"Morning after, things are still good!</li>    
+            </ul>
+            <p>Keeping in touch with people is an important safety precaution.   Being able to notify people quickly also helps in many situations. It's important to let people know that "IMOK!"</p>
+            <br/>
+            <h3>Girl-Power-5 Team</h3>
+            <p>We are always looking to learn and improve our applications, please feel free to reach out with suggestions or ideas for improvement in future versions.</p>
+            <ul>
+                <li><a href="claudiaalpert@gmail.com"></a>Claudia</li>
+                <li><a href="emilyblair96@gmail.com"></a>Emily</li>
+                <li><a href="amber.park@northwestern.edu"></a>Amber</li>
+                <li><a href="jen.doyle5254@gmail.com"></a>Jen</li>
+                <li><a href="melissaward953@gmail.com"></a>Melissa</li>
+               
+            </ul>
+            <p>For more information on this project or others we have made, [here is a link](https://github.com/girl-power-5) to our organization on GitHub.</p>
+            <p>Check out the Girl-Power 5 Members personal projects on GitHub:</p>
+            <ul>
+                <li>[Claudia Alpert](https://github.com/Calpert)</li>
+                <li>[Emily Blair](https://github.com/emblair96)</li>
+                <li>[Amber Chiodini](https://github.com/amberchiodini)</li>
+                <li>[Jen Doyle](https://github.com/Jdoyle5254)</li>
+                <li>[Melissa Ward](https://github.com/mw18)</li>
+            </ul>
+
+
+            </article>
+
+        </div>
+    )
+
+
+}
+
+
+
